cf319b6dab87e4b36f1f183ce3b1ba98114ab662
CristhyanG <cristhyangiovannemg@gmail.com>
2024-06-10 22:32:51 -0300

    Criação de uma aplicação feita para editar imagens através do celular feito numa build anterior do react-native

---
+++ MoonTAG/App.js
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+const placeholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+
+        <ImageViewer placeholderImageSource={placeholderImage}/>
+
+      </View>
+      <Text style={{color: '#fff'}}>um giovannni</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
